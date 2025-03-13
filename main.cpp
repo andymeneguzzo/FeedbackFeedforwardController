12748659a5bd4bd32dcc8c6a03950d9a2ff1c21c
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-03-13 16:36:02 +0100

    FeedbackController implementation

--- main.cpp
+++ main.cpp
@@ -11,3 +11,27 @@ class InterfaceController {
         virtual double compute(double setpoint, double measurement, double dt) = 0;
         virtual ~InterfaceController() {}
 };
+
+// IMPLEMENT A FEEDBACK CONTROLLER
+class FeedbackController : public InterfaceController {
+    private:
+        double Kp, Ki, Kd;
+        double integral;
+        double prev_error;
+        bool first_run;
+    public:
+        FeedbackController(double Kp, double Ki, double Kd)
+        : Kp(Kp), Ki(Ki), Kd(Kd), integral(0.0), prev_error(0.0), first_run(true) {}
+
+        double compute(double setpoint, double measurement, double dt) override {
+            double error = setpoint - measurement;
+            integral += error * dt;
+
+            double derivative = first_run ? 0.0 : (error - prev_error)/dt;
+            first_run = false;
+            prev_error = error;
+
+            // SUMMING ALL TERMS
+            return Kp*error + Ki*integral + Kd*derivative;
+        }
+};
