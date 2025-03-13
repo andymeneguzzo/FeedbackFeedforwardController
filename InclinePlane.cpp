0188c5012bfaf061a82be7b98b90d3892c2f675d
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-03-14 00:05:33 +0100

    Feedback controller

--- InclinePlane.cpp
+++ InclinePlane.cpp
@@ -3,3 +3,35 @@
 #include <iomanip>
 #include <fstream>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+class InterfaceController {
+    public:
+        virtual double compute(double setpoint, double measurement, double dt) = 0;
+        virtual ~InterfaceController() {}
+};
+
+// FEEDBACK CONTROLLER PID
+class FeedbackController : public InterfaceController {
+    private:
+        double Kp, Ki, Kd;
+        double integral;
+        double prev_error;
+        bool first_run;
+    public:
+        FeedbackController(double Kp, double Ki, double Kd)
+        : Kp(Kp), Ki(Ki), Kd(Kd), integral(0.0), prev_error(0.0), first_run(true) {}
+
+        double compute(double setpoint, double measurement, double dt) override {
+            double error = setpoint - measurement;
+            integral += error * dt;
+
+            double derivative = first_run ? 0.0 : (error - prev_error)/dt;
+            first_run = false;
+            prev_error = error;
+
+            return Kp*error + Ki*integral + Kd*derivative;
+        }
+};
